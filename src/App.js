e6c671799023ef4c5e429c9c03d7bde7f055389d
wingsKingdom <wingsshadow0627@gmail.com>
2021-07-13 09:10:17 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';  
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "This setState function"
+    };
+    this.updateSetState = this.updateSetState.bind(this);
+    this.forceUpdateState = this.forceUpdateState.bind(this);
+
+    this.findDomNodeHandler1 = this.findDomNodeHandler1.bind(this);
+    this.findDomNodeHandler2 = this.findDomNodeHandler2.bind(this);
+  }
+
+  updateSetState = () => {
+    this.setState({msg: "state updated!"});
+  };
+
+  forceUpdateState = () => {
+    this.forceUpdate();
+  };
+
+  findDomNodeHandler1 = () => {
+    var myDivOne = document.getElementById("myDivOne");
+    
+    ReactDOM.findDOMNode(myDivOne).style.color = 'red';
+  }
+  
+  findDomNodeHandler2 = () => {
+    var myDivTwo = document.getElementById('myDivTwo');
+    ReactDOM.findDOMNode(myDivTwo).style.color = 'blue';
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This Component API TEST</h1>
+        <div>
+          <h1>
+            {this.state.msg} <br />
+            <button onClick={this.updateSetState}>SET STATE</button>
+          </h1>
+        </div>
+        <div>
+          <h1>Example to generate random number</h1>
+          <h1>Random number: {Math.random()}</h1>
+          <br />
+          <button onClick={this.forceUpdateState}>ForceUp</button>
+        </div>
+        <div>
+          <h1>ReactJS Find DOM NODE Example</h1>
+          <button onClick={this.findDomNodeHandler1}>FIND_DOM_NODE1</button>
+          <button onClick={this.findDomNodeHandler2}>FIND_DOM_NODE2</button>
+          <h3 id="myDivOne">JTP-NODE1</h3>
+          <h3 id="myDivTwo">JTP-NODE2</h3>
+        </div>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
