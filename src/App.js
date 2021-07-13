ddab98cf46c5e5db7214c4f2cf9a65cf4dd26329
wingsKingdom <wingsshadow0627@gmail.com>
2021-07-13 13:39:24 +0200

    second again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';  
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+
+import CondRender from './CondRender';
 
 class App extends React.Component {
   constructor() {
@@ -43,20 +45,24 @@ class App extends React.Component {
             {this.state.msg} <br />
             <button onClick={this.updateSetState}>SET STATE</button>
           </h1>
-        </div>
+        </div><hr />
         <div>
           <h1>Example to generate random number</h1>
           <h1>Random number: {Math.random()}</h1>
           <br />
           <button onClick={this.forceUpdateState}>ForceUp</button>
-        </div>
+        </div><hr />
         <div>
           <h1>ReactJS Find DOM NODE Example</h1>
           <button onClick={this.findDomNodeHandler1}>FIND_DOM_NODE1</button>
           <button onClick={this.findDomNodeHandler2}>FIND_DOM_NODE2</button>
           <h3 id="myDivOne">JTP-NODE1</h3>
           <h3 id="myDivTwo">JTP-NODE2</h3>
-        </div>
+        </div><hr />
+        <div>
+          <h1>This is Conditional Render Sample</h1>
+          <CondRender />
+        </div><hr />
       </div>
       
     );
